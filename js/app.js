437203d94bc1481c2300c628af1f7c43db99b7da
Max0u7 <Mr_Max0u7@protonmail.com>
2021-09-16 18:51:25 -0300

    fig browser compatibility, and some responsive

--- js/app.js
+++ js/app.js
@@ -20,3 +20,27 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+function hasClass(ele, cls) {
+	return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+
+document.getElementById("sun").addEventListener("click", function () {
+	var r = document.querySelector(':root');
+	// Get the styles (properties and values) for the root
+	var rs = getComputedStyle(r);
+	console.log(rs)
+	const btn = document.getElementById('sun')
+	const body = document.getElementById('body')
+	const title = document.getElementById('title')
+
+	console.log(title.style.backgroundColor = 'black')
+	btn.classList.toggle('fa-sun');
+	btn.classList.toggle('fa-moon');
+	body.classList.toggle('bg-dark');
+	body.classList.toggle('bg-light');
+	body.classList.toggle('text-dark');
+	body.classList.toggle('text-light');
+
+});
